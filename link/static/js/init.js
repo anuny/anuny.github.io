227c8e75ff01d9dcdd8d396594e52d8c94fd35bf
YangFei <yangfei88@live.cn>
2019-11-12 10:51:55 +0800

    Update init.js

--- link/static/js/init.js
+++ link/static/js/init.js
@@ -18,7 +18,7 @@ seajs.use('main',
 		$(window).resize(function() {
 			layout.mainWidth($('.menu'), $('.container'));
 		});
-		data.ajax('https://raw.githubusercontent.com/anuny/anuny.github.io/master/link/static/data/data.json');	
+		data.ajax('http://www.thankwork.cn/link/data.json');	
 		user.login();
 		var len=$('#main>li>ul>li').length;
 		var tit=$('title');
